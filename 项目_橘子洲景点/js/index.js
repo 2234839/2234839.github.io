f340e332ae27d0793dd61cea4a2bc882c2fbbb1a
崮生 <2234839456@qq.com>
2017-11-23 06:04:06 -0600

    Add files via upload

--- 项目_橘子洲景点/js/index.js
+++ 项目_橘子洲景点/js/index.js
@@ -1,5 +1,5 @@
 window.onload=function(){
-
+	tiaozheng()
 }	
 
 
@@ -12,7 +12,6 @@ function tiaozheng(){
 	
 	//y+=1000
 	setInterval('a()',20)	//1300*8666
-	setInterval('设置横坐标()',150)
 	
 	console.log(y)
 }
@@ -20,26 +19,18 @@ var y_z=3	//y的增量
 
 var sx=0
 var sy=0
-var swidth=1300
-var sheight=1000
 var x=0
 var y=0
 
 var 前进状态=true
-//岛的折线形态
+
+//岛的折线形态数组
 var zb=[[642,19307],	//岛最下端中点
 		[868,17169],
 		[1102,12425],
 		[1614,6954],	//桥的中点
 		[2498,3180],
 		[2802,2144]]	//最顶端中点
-
-function zhuanhaun(){
-	前进状态=!前进状态
-	y_z=-y_z
-	console.log(y_z)
-	console.log("=>:"+(y+=y_z))
-}
 
 function 设置横坐标(){
 	var Y=20000-y
@@ -53,8 +44,6 @@ function 设置横坐标(){
 			i=4
 		if(Y<zb[4][1])
 			i=5
-		if(Y<zb[4][1])
-			y_z=-3
 	}
 	else{
 		if(Y>zb[5][1])
@@ -65,27 +54,25 @@ function 设置横坐标(){
 			i=2
 		if(Y>zb[2][1])
 			i=1
-		if(Y>zb[1][1])
-			y_z=3
 	}
 	
-	//console.log(y_z)
-	
+	//直线方程
 	var X1=zb[i][0]
 	var Y1=zb[i][1]
 	
 	var X2=zb[i-1][0]
 	var Y2=zb[i-1][1]
+	
 	var A = Y2 - Y1
 	var B = X1 - X2
 	var C = X2*Y1 - X1*Y2
 	
 	sx=(-B*Y-C)/A-900
-	
-	//console.log(sx)
+
 }
 
 function a(){
+	设置横坐标()
 	y+=y_z
-	ctx.drawImage(img,sx,19307-y,3000,1770,x,0,3000,1770);
+	ctx.drawImage(img,sx,18307-y,3000,2300,x,0,3000,1600);
 }
